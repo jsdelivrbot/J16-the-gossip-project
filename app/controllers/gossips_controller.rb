84a5832e51d2785d0a67f91bca5383e8950c9475
Clement <clementlecuillier@hotmail.fr>
2018-01-31 17:32:52 +0100

    finish the gossip project

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -10,12 +10,34 @@ class GossipsController < ApplicationController
 
   def create
     @gossip = Gossip.new(gossip_params)
-    @gossip.save
-    redirect_to @gossip
+    if @gossip.save
+      redirect_to @gossip
+    else
+      render 'new'
+    end
   end
 
   def show
     @gossip = Gossip.find(params[:id])
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :gossips
   root 'gossips#index'
   get 'gossips/show'
-  get '/gossips/new'
+  get 'gossips/new'
+  get 'gossips/edit'
 end
