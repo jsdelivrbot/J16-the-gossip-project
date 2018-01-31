86ce4cfd9dbe397603c72e5b8a6f8d261cea4b0f
Clement <clementlecuillier@hotmail.fr>
2018-01-31 16:46:00 +0100

    show

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,14 +1,26 @@
 class GossipsController < ApplicationController
 
   def index
+    @gossips = Gossip.all
   end
 
   def new
     @gossip = Gossip.new
   end
 
-  def show
-    @gossips = Gossip.all
+  def create
+    @gossip = Gossip.new(gossip_params)
+    @gossip.save
+    redirect_to @gossip
   end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  private
+    def gossip_params
+      params.require(:gossip).permit(:anonymous_author, :content)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :gossips
   root 'gossips#index'
+  get 'gossips/show'
   get '/gossips/new'
 end
